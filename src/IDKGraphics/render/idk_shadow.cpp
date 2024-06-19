9bd7667bd6177d05a60dde29f452a9edd52e5c0a
mellic03 <mellic03@gmail.com>
2024-06-19 20:34:59 +1000

    Directory restructure

--- src/IDKGraphics/render/idk_shadow.cpp
+++ src/IDKGraphics/render/idk_shadow.cpp
@@ -48,37 +48,38 @@ idk::RenderEngine::shadowpass_dirlights()
 
     // User-created shadow render queues
     // -----------------------------------------------------------------------------------------
-    auto &light = getDirlight(0);
-
-    for (idk::RenderQueue &queue: m_user_shadow_queues)
+    for (auto &light: m_dirlights)
     {
-        const auto &commands = queue.genDrawCommands(
-            *m_DrawIndirectData, m_model_allocator
-        );
-
-        if (commands.size() == 0)
+        for (idk::RenderQueue &queue: m_user_shadow_queues)
         {
-            continue;
+            const auto &commands = queue.genDrawCommands(
+                *m_DrawIndirectData, m_model_allocator
+            );
+
+            if (commands.size() == 0)
+            {
+                continue;
+            }
+
+            m_DrawCommandBuffer.bufferSubData(
+                0, commands.size()*sizeof(idk::glDrawCmd), commands.data()
+            );
+
+            m_DrawIndirectSSBO.update(*m_DrawIndirectData);
+
+
+            auto &program = getProgram(queue.name);
+            program.bind();
+            program.set_mat4("un_lightspacematrix", light.transform);
+
+            gl::multiDrawElementsIndirect(
+                GL_TRIANGLES,
+                GL_UNSIGNED_INT,
+                nullptr,
+                commands.size(),
+                sizeof(idk::glDrawCmd)
+            );
         }
-
-        m_DrawCommandBuffer.bufferSubData(
-            0, commands.size()*sizeof(idk::glDrawCmd), commands.data()
-        );
-
-        m_DrawIndirectSSBO.update(*m_DrawIndirectData);
-
-
-        auto &program = getProgram(queue.name);
-        program.bind();
-        program.set_mat4("un_lightspacematrix", light.transform);
-
-        gl::multiDrawElementsIndirect(
-            GL_TRIANGLES,
-            GL_UNSIGNED_INT,
-            nullptr,
-            commands.size(),
-            sizeof(idk::glDrawCmd)
-        );
     }
     // -----------------------------------------------------------------------------------------
 
